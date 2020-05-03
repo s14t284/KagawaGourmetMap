84c19cab59dd87a3db70b6dd58e85375e577761e
Ryuya Ikeda <rikeda71@gmail.com>
2020-05-04 02:25:50 +0900

    Edit the map component

--- frontend/src/components/map.tsx
+++ frontend/src/components/map.tsx
@@ -1,11 +1,21 @@
 import React from "react";
-import L from 'leaflet';
+import L, { marker } from 'leaflet';
 import "leaflet/dist/leaflet.css";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import { LatLngExpression } from "leaflet";
 import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png'
 import iconUrl from 'leaflet/dist/images/marker-icon.png'
 import shadowUrl from 'leaflet/dist/images/marker-shadow.png'
+
+const mapStyle: React.CSSProperties = {
+    height: "70vh",
+    width: "70vw",
+    marginBottom: 0,
+    marginRight: "1rem",
+    marginLeft: "auto",
+    position: "relative"
+};
+
 const rewriteIconState = () => {
     /* eslint-disable */
     delete (L.Icon.Default.prototype as any)._getIconUrl;
@@ -17,32 +27,52 @@ const rewriteIconState = () => {
     /* eslint-disable */
 }
 
-
-type gourmetMapProps = {
-    zoomValue: number,
-    centorPosition: LatLngExpression,
-    markers: Array<LatLngExpression>
+export type MarkerType = {
+    position: LatLngExpression,
+    popup: string
 };
 
+type MarkersType = {
+    markers: Array<MarkerType>
+}
 
-const GourmetMap : React.FC<gourmetMapProps> = (props) => {
+type GourmetMapProps = {
+    zoomValue: number,
+    centorPosition: LatLngExpression,
+    markers: Array<MarkerType>
+}
+
+const MyMarker: React.FC<MarkerType> = (props) => {
+    return (
+      <Marker position={props.position} key={props.popup}>
+          <Popup>{props.popup}</Popup>
+      </Marker>
+    )
+}
+
+const Markers: React.FC<MarkersType> = (props) => {
+    const markers: Array<MarkerType> = props.markers;
+    return (
+        <div id="markers">
+            {markers.map((marker, i) => {return (<MyMarker {...marker} key={i} />)})}
+        </div>
+    )
+}
+
+const GourmetMap : React.FC<GourmetMapProps> = (props) => {
     const zoomValue = props.zoomValue;
     const centorPosition = props.centorPosition;
-    const markers: Array<LatLngExpression> = props.markers;
-    const style ={height: "80vh", weight: "80vh"};
 
     rewriteIconState();
 
     return (
         <div id="map">
-            <Map zoom={zoomValue} center={centorPosition} style={style}>
+            <Map zoom={zoomValue} center={centorPosition} style={mapStyle}>
                 <TileLayer
                 url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                 />
-            <Marker position={markers[0]}>
-                <Popup>popUp</Popup>
-            </Marker>
+            <Markers markers={props.markers} />
             </Map>
         </div>
     )
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import GourmetMap from "./components/map";
+import GourmetMap, { MarkerType } from "./components/map";
+import Shops from "./components/shops";
 
 const root = document.getElementById("root");
 const Root: React.FC = () => {
+  const markers: Array<MarkerType> = [
+    { position: [35.362222, 138.731388], popup: "first marker" },
+    { position: [36.362222, 138.731388], popup: "second marker" },
+  ];
   return (
     <div>
       <h1>{"Hello World"}</h1>
       <GourmetMap
-        zoomValue={13}
-        centorPosition={[35.562222, 139.731388]}
-        markers={[[35.362222, 138.731388]]}
+        zoomValue={7}
+        centorPosition={[35.562222, 138.731388]}
+        markers={markers}
       />
+      <Shops request_url="http://localhost:4010"/>
     </div>
   );
 };
