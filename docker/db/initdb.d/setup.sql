a6e587af9d9e64a7191398718a91f00ee73af11a
maru0 <marutforwork0321@gmail.com>
2020-05-04 01:22:19 +0900

    fix table

--- docker/db/initdb.d/setup.sql
+++ docker/db/initdb.d/setup.sql
@@ -1,23 +1,15 @@
 CREATE TABLE Shops(
   shop_id        PRIMARY KEY,
   shop_name      VARCHAR(40),
+  review_point   Decimal,
+  geom           geography(POINT,4326) NOT NULL
 );
+
+CREATE TYPE day_enum AS ENUM ('月','火','水','木','金','土','日');
 
 CREATE TABLE Shop_Time(
-  sales_day      VARCHAR(40),
-  start_time     Time,
-  end_time       Time,
+  sales_day      day_enum,
+  start_time     Time NOT NULL,
+  end_time       Time NOT NULL,
   foreign key(shop_id) references Shops(shop_id)
 );
-
-CREATE TABLE Shop_Point(
-  shop_point      Decimal,
-  foreign key(shop_id) references Shops(shop_id)
-);
-
-CREATE TABLE Shop_Geo(
-  geo_lat      Decimal,
-  get_lng      Decimal,
-  foreign key(shop_id) references Shops(shop_id)
-);
-
