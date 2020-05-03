8e6a2c093aa14cc475a321ab52fb43755422e848
maru0 <marutforwork0321@gmail.com>
2020-05-04 00:57:48 +0900

    create table

--- docker/db/initdb.d/setup.sql
+++ docker/db/initdb.d/setup.sql
@@ -1,6 +1,23 @@
-CREATE TABLE Users(
-  account_id        SERIAL PRIMARY KEY,
-  account_name      VARCHAR(20),
-  email             VARCHAR(100),
-  password    CHAR(64)
+CREATE TABLE Shops(
+  shop_id        PRIMARY KEY,
+  shop_name      VARCHAR(40),
 );
+
+CREATE TABLE Shop_Time(
+  sales_day      VARCHAR(40),
+  start_time     Time,
+  end_time       Time,
+  foreign key(shop_id) references Shops(shop_id)
+);
+
+CREATE TABLE Shop_Point(
+  shop_point      Decimal,
+  foreign key(shop_id) references Shops(shop_id)
+);
+
+CREATE TABLE Shop_Geo(
+  geo_lat      Decimal,
+  get_lng      Decimal,
+  foreign key(shop_id) references Shops(shop_id)
+);
+
